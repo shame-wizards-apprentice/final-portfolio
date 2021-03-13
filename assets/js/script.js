412bb75aea9d1a95ea98820823aefc0a1d5d6d3d
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-13 11:49:57 -0800

    build most project cards, added dark and light mode designs

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Switch between dark and light mode
+$("#dark").on("click", () => {
+    $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)" });
+    $("#projects").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
+    $(".description").css({ "color": "rgba(255, 255, 255, .8)" });
+    $(".tech-list-heading").css({ "color": "rgba(255, 255, 255, .8)" });
+    $(".project-title").css({ "color": "rgba(255, 255, 255, .8)" });
+    $(".project-blurb").css({ "color": "rgba(255, 255, 255, .8)" });
+    $(".close-btn").css({ "color": "rgba(255, 255, 255, .8)" });
+    $(".card-title").css({ "color": "rgba(255, 255, 255, .8)" });
+    $(".card-content").css({ "background-color": "#121212" })
+    $(".card-reveal").css({ "background-color": "#121212" })
+
+})
+
+$("#light").on("click", () => {
+    $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)" });
+    $("#projects").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
+    $(".card-content").css({ "background-color": "lightgrey" });
+    $(".card").css({ "border": "solid 5px black;" });
+    $(".description").css({ "color": "black" });
+    $(".tech-list-heading").css({ "color": "black" });
+    $(".project-title").css({ "color": "black" });
+    $(".project-blurb").css({ "color": "black" });
+    $(".close-btn").css({ "color": "black" });
+    $(".card-title").css({ "color": "black" });
+    $(".card-reveal").css({ "background-color": "white" })
+})
