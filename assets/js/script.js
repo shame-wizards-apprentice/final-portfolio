f8458d2e8c5bac97c414d0579f2ac30b9112cdf1
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-13 23:46:50 -0800

    stored mode in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,32 @@
 // Enable scrollspy
 $(document).ready(() => {
     $('.scrollspy').scrollSpy();
+    switch (localStorage.getItem("mode")) {
+        case "dark":
+            $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)", "color": "rgba(255, 255, 255, .8)" });
+            $(".container").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
+            $(".card-content").css({ "background-color": "#121212" });
+            $("#contact").css({ "background-color": "#121212" });
+            $(".card-reveal").css({ "background-color": "#121212" });
+            break;
+        case "light":
+            $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
+            $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
+            $(".card-content").css({ "background-color": "lightgrey" });
+            $("#contact").css({ "background-color": "lightgrey" });
+            $(".card").css({ "border": "solid 5px black;" });
+            $(".card-reveal").css({ "background-color": "white" });
+            break;
+        default:
+            $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
+            $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
+            $(".card-content").css({ "background-color": "lightgrey" });
+            $("#contact").css({ "background-color": "lightgrey" });
+            $(".card").css({ "border": "solid 5px black;" });
+            $(".card-reveal").css({ "background-color": "white" });
+    }
 });
 
-$(window).scroll(e => {
-    const $el = $("#initialization");
-    const isPositionFixed = ($el.css("position") == "fixed");
-    if ($(this).scrollTop() > 200 && !isPositionFixed) {
-        $el.css({ "position": "fixed", "top": "0px" });
-    }
-    if ($(this).scrollTop() < 200 && isPositionFixed) {
-        $el.css({ "position": "static", "top": "0px" });
-    }
-});
 
 // Switch between dark and light mode
 $("#dark").on("click", () => {
@@ -22,7 +36,9 @@ $("#dark").on("click", () => {
     // Projects page
     $(".card-content").css({ "background-color": "#121212" });
     $("#contact").css({ "background-color": "#121212" });
-    $(".card-reveal").css({ "background-color": "#121212" })
+    $(".card-reveal").css({ "background-color": "#121212" });
+
+    localStorage.setItem("mode", "dark");
 
 })
 
@@ -34,5 +50,6 @@ $("#light").on("click", () => {
     $(".card-content").css({ "background-color": "lightgrey" });
     $("#contact").css({ "background-color": "lightgrey" });
     $(".card").css({ "border": "solid 5px black;" });
-    $(".card-reveal").css({ "background-color": "white" })
+    $(".card-reveal").css({ "background-color": "white" });
+    localStorage.setItem("mode", "light");
 })
