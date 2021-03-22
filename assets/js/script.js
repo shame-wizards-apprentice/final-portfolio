aea0e454cca06ca6a0e82280698e325c4a7fd65a
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-21 21:00:41 -0700

    consolidated dark/light mode buttons into one

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(() => {
             $("#contact").css({ "background-color": "#121212" });
             $(".card-reveal").css({ "background-color": "#121212" });
             break;
+
         case "light":
             $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
             $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
@@ -18,6 +19,7 @@ $(document).ready(() => {
             $(".card").css({ "border": "solid 5px black;" });
             $(".card-reveal").css({ "background-color": "white" });
             break;
+
         default:
             $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
             $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
@@ -30,27 +32,48 @@ $(document).ready(() => {
 
 
 // Switch between dark and light mode
-$("#dark").on("click", () => {
-    // Global
-    $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)", "color": "rgba(255, 255, 255, .8)" });
-    $(".container").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
-    // Projects page
-    $(".card-content").css({ "background-color": "#121212" });
-    $("#contact").css({ "background-color": "#121212" });
-    $(".card-reveal").css({ "background-color": "#121212" });
+$("#mode").on("click", () => {
+    switch (localStorage.getItem("mode")) {
+        case "light":
+            // Button
+            $("#mode").removeClass("grey darken-4").addClass("deep-purple lighten-5 black-text").text("Light Mode")
+            // Global
+            $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)", "color": "rgba(255, 255, 255, .8)" });
+            $(".container").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
+            // Projects page
+            $(".card-content").css({ "background-color": "#121212" });
+            $("#contact").css({ "background-color": "#121212" });
+            $(".card-reveal").css({ "background-color": "#121212" });
+            localStorage.setItem("mode", "dark");
+            break;
 
-    localStorage.setItem("mode", "dark");
+        case "dark":
+            // Button
+            $("#mode").removeClass("deep-purple lighten-5 black-text").addClass("grey darken-4").text("Dark Mode")
+            // Global
+            $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
+            $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
+            // Projects page
+            $(".card-content").css({ "background-color": "lightgrey" });
+            $("#contact").css({ "background-color": "lightgrey" });
+            $(".card").css({ "border": "solid 5px black;" });
+            $(".card-reveal").css({ "background-color": "white" });
+            localStorage.setItem("mode", "light");
+            break;
+
+        default:
+            // Button
+            $("#mode").removeClass("grey darken-4").addClass("deep-purple lighten-5 black-text").text("Light Mode")
+            // Global
+            $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
+            $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
+            // Projects page
+            $(".card-content").css({ "background-color": "lightgrey" });
+            $("#contact").css({ "background-color": "lightgrey" });
+            $(".card").css({ "border": "solid 5px black;" });
+            $(".card-reveal").css({ "background-color": "white" });
+            localStorage.setItem("mode", "light");
+    }
 
 })
 
-$("#light").on("click", () => {
-    // Global
-    $("body").css({ "background-image": "linear-gradient(to top, #8b8299, #e4e5e6)", "color": "black" });
-    $(".container").css({ "background-image": "linear-gradient(to bottom, #e4e5e6, white, #8b8299)" });
-    // Projects page
-    $(".card-content").css({ "background-color": "lightgrey" });
-    $("#contact").css({ "background-color": "lightgrey" });
-    $(".card").css({ "border": "solid 5px black;" });
-    $(".card-reveal").css({ "background-color": "white" });
-    localStorage.setItem("mode", "light");
-})
