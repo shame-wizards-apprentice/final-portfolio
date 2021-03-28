cf6c90913b5d3156c6a6f89e37c754e68a6d33fe
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-28 10:52:18 -0700

    DRY up code in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,86 @@
 $(document).ready(() => {
+    aboutMe();
     // Fetch last used mode from local storage, or display light mode if no record
-    $("#projects").css("display", "none");
     switch (localStorage.getItem("mode")) {
         case "dark":
-            $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)", "color": "rgba(255, 255, 255, .8)" });
-            $(".container").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
-            $(".card-content").css({ "background-color": "#121212" });
-            $(".card-reveal").css({ "background-color": "#121212" });
-            $("#mode").text("Light Mode");
-            $(".btn-small").removeClass("grey darken-4").addClass("deep-purple lighten-5 black-text");
+            darkMode();
             break;
 
         case "light":
-            $("body").css({ "background-image": "linear-gradient(to top, #D4D3DD, #EFEFBB)", "color": "black" });
-            $(".container").css({ "background-image": "linear-gradient(to bottom, #EFEFBB, white, #D4D3DD)" });
-            $(".card-content").css({ "background-color": "lightgrey" });
-            $(".card").css({ "border": "solid 5px black;" });
-            $(".card-reveal").css({ "background-color": "white" });
-            $("#mode").text("Dark Mode");
-            $(".btn-small").removeClass("deep-purple lighten-5 black-text").addClass("grey darken-4");
+            lightMode();
             break;
 
         default:
-            $("body").css({ "background-image": "linear-gradient(to top, #D4D3DD, #EFEFBB)", "color": "black" });
-            $(".container").css({ "background-image": "linear-gradient(to bottom, #EFEFBB, white, #D4D3DD)" });
-            $(".card-content").css({ "background-color": "lightgrey" });
-            $(".card").css({ "border": "solid 5px black;" });
-            $(".card-reveal").css({ "background-color": "white" });
-            $("#mode").text("Dark Mode");
-            $(".btn-small").removeClass("deep-purple lighten-5 black-text").addClass("grey darken-4");
+            lightMode();
     }
 });
 
+// Set dark mode
+const darkMode = () => {
+    $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)", "color": "rgba(255, 255, 255, .8)" });
+    $(".container").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
+    $(".card-content").css({ "background-color": "#121212" });
+    $(".card-reveal").css({ "background-color": "#121212" });
+    $("#mode").text("Light Mode");
+    $(".btn-small").removeClass("grey darken-4").addClass("deep-purple lighten-5 black-text");
+    localStorage.setItem("mode", "dark");
+}
+
+// Set light mode
+const lightMode = () => {
+    $("body").css({ "background-image": "linear-gradient(to top, #D4D3DD, #EFEFBB)", "color": "black" });
+    $(".container").css({ "background-image": "linear-gradient(to bottom, #EFEFBB, white, #D4D3DD)" });
+    $(".card-content").css({ "background-color": "lightgrey" });
+    $(".card").css({ "border": "solid 5px black;" });
+    $(".card-reveal").css({ "background-color": "white" });
+    $("#mode").text("Dark Mode");
+    $(".btn-small").removeClass("deep-purple lighten-5 black-text").addClass("grey darken-4");
+    localStorage.setItem("mode", "light");
+}
+
+// Display about me
+const aboutMe = () => {
+    $("#about").css("display", "block");
+    $("#projects").css("display", "none");
+    $("#info").text("My Work");
+}
+
+// Display my work
+const myWork = () => {
+    $("#about").css("display", "none");
+    $("#projects").css("display", "block");
+    $("#info").text("About Me");
+}
 
 // Switch between dark and light mode
 $("#mode").on("click", () => {
     switch (localStorage.getItem("mode")) {
         case "light":
-            // Global
-            $("#mode").text("Light Mode");
-            $(".btn-small").removeClass("grey darken-4").addClass("deep-purple lighten-5 black-text");
-            $("body").css({ "background-image": "linear-gradient(to top, #000000, #434343)", "color": "rgba(255, 255, 255, .8)" });
-            $(".container").css({ "background-image": "linear-gradient(to bottom, #434343, #121212, #000000)" });
-            // Projects page
-            $(".card-content").css({ "background-color": "#121212" });
-            $(".card-reveal").css({ "background-color": "#121212" });
-            localStorage.setItem("mode", "dark");
+            darkMode();
             break;
 
         case "dark":
-            // Global
-            $("#mode").text("Dark Mode");
-            $("body").css({ "background-image": "linear-gradient(to top, #D4D3DD, #EFEFBB)", "color": "black" });
-            $(".container").css({ "background-image": "linear-gradient(to bottom, #EFEFBB, white, #D4D3DD)" });
-            // Projects page
-            $(".card-content").css({ "background-color": "lightgrey" });
-            $(".card").css({ "border": "solid 5px black;" });
-            $(".card-reveal").css({ "background-color": "white" });
-            $(".btn-small").removeClass("deep-purple lighten-5 black-text").addClass("grey darken-4");
-            localStorage.setItem("mode", "light");
+            lightMode();
             break;
 
         default:
-            // Global
-            $("#mode").text("Light Mode");
-            $("body").css({ "background-image": "linear-gradient(to top, #D4D3DD, #EFEFBB)", "color": "black" });
-            $(".container").css({ "background-image": "linear-gradient(to bottom, #EFEFBB, white, #D4D3DD)" });
-            $(".btn-small").removeClass("grey darken-4").addClass("deep-purple lighten-5 black-text");
-            // Projects page
-            $(".card-content").css({ "background-color": "lightgrey" });
-            $(".card").css({ "border": "solid 5px black;" });
-            $(".card-reveal").css({ "background-color": "white" });
-            localStorage.setItem("mode", "light");
+            darkMode();
     }
 
 })
 
-// Show "About Me", hide "About My Work"
-$("#about-btn").on("click", () => {
-    $("#about").css("display", "block");
-    $("#projects").css("display", "none");
+// Switch between "About Me" and "About My Work"
+$("#info").on("click", () => {
+    switch ($("#info").text()) {
+        case "About Me":
+            aboutMe()
+            break;
+        case "My Work":
+            myWork();
+            break;
+        default:
+            aboutMe();
+    }
 })
 
-// Show "About My Work", hide "About Me"
-$("#projects-btn").on("click", () => {
-    $("#about").css("display", "none");
-    $("#projects").css("display", "block");
-})
 
